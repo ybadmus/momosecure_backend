8760e19a87c85700f061a7e33e46784bb7321bd5
ybadmus <yusif.badmus@gmail.com>
2023-12-12 21:43:58 +0000

    add payment timeline service

--- app/services/payment/timeline_service.rb
+++ app/services/payment/timeline_service.rb
@@ -1,28 +1,32 @@
-class Payment::TimelineService < ApplicationService
-  def initialize(payment_transaction:)
-    @payment_transaction = payment_transaction
-    @payment_transaction_timeline = @payment_transaction.payment_transaction_timeline
-  end
+# frozen_string_literal: true
 
-  def perform
-    return unless status_column_exists?
-    return if @business_order_timeline.blank?
-    return if status_already_filled?
+module Payment
+  class TimelineService
+    def initialize(payment_transaction:)
+      @payment_transaction = payment_transaction
+      @payment_transaction_timeline = @payment_transaction.payment_transaction_timeline
+    end
 
-    @business_order_timeline.update!({ status_column_name => Time.zone.now })
-  end
+    def perform
+      return unless status_column_exists?
+      return if @payment_transaction_timeline.blank?
+      return if status_already_filled?
 
-  private
+      @payment_transaction_timeline.update!({ status_column_name => Time.zone.now })
+    end
 
-  def status_column_name
-    "#{@payment_transaction.status}_at"
-  end
+    private
 
-  def status_column_exists?
-    BusinessOrderTimeline.column_names.include?(status_column_name)
-  end
+    def status_column_name
+      "#{@payment_transaction.status}_at"
+    end
 
-  def status_already_filled?
-    @payment_transaction_timeline.read_attribute(status_column_name).present?
+    def status_column_exists?
+      BusinessOrderTimeline.column_names.include?(status_column_name)
+    end
+
+    def status_already_filled?
+      @payment_transaction_timeline.read_attribute(status_column_name).present?
+    end
   end
 end
